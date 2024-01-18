ea1c58446434223e329cadb1c4a2dc03912ba3fc
rotoos <87291030@qq.com>
2024-01-18 17:02:59 +0800

    init

--- stc/public/index.php
+++ stc/public/index.php
@@ -2,6 +2,13 @@
 
 use think\admin\service\RuntimeService;
 
+//支持跨域
+header("Access-Control-Allow-Origin:*");
+header('Access-Control-Allow-Methods:GET,POST,OPTIONS');
+header('Access-Control-Expose-Headers:Content-Disposition');
+header('Content-Type:text/event-stream');
+header('Access-Control-Allow-Headers:x-requested-with,Authorization,Content-Type,Token,Api-Token,Jwt-Token');
+
 // 加载基础文件
 require __DIR__ . '/../vendor/autoload.php';
 
